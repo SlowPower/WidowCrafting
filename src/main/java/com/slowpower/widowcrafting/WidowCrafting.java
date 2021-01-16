f2ac4d2bdd392cc9d4a29d40ac082e3b39faa3f9
SlowPower <loganp86@icloud.com>
2021-01-16 12:04:20 -0500

    Initial commit

---
+++ src/main/java/com/slowpower/widowcrafting/WidowCrafting.java
@@ -0,0 +1,16 @@
+package com.slowpower.widowcrafting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WidowCrafting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
